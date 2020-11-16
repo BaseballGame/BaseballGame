037f4664b4f98523b800e79a88f13d789bc0915a
jpferner <74560914+jpferner@users.noreply.github.com>
2020-11-16 13:10:23 -0500

    Add files via upload

---
+++ RunBaseballGame.java
@@ -0,0 +1,23 @@
+import javax.swing.SwingUtilities;
+//////////////////////////////////////////////////////////////////////
+//CSC2220
+//Programming Assignment 6-7
+/////////////////////////////////////////////////////////////////////
+/*MAIN CLASS*/
+/////////////////////////////////////////////////////////////////////
+public class RunBaseballGame {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.getFrame().setVisible(true);
+					// window.resizeFrames();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		});
+	}
+}
